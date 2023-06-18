155623c7f9b5b2a82811ea58e4d8278966d60180
Alexander Herbrich <aherbrich32@gmail.com>
2023-06-18 18:25:22 +0200

    Added database to store all chess positions (and corresponding winrates) which occur in list of given chess games!

--- src/parse.c
+++ src/parse.c
@@ -2,7 +2,6 @@
 #include "../include/prettyprint.h"
 #include "../include/zobrist.h"
 #include <string.h>
-#include <math.h>
 
 #define WINWHITE 1
 #define DRAW 0
@@ -18,6 +17,94 @@ typedef struct _chessgame_t{
     char* movelist;
     int winner;
 } chessgame_t;
+
+typedef struct _databaseentry_t {
+    uint64_t hash;
+    struct _databaseentry_t *next;
+
+    int seen;
+    int white_won;
+    int black_won;
+    int draw;
+} databaseentry_t;
+
+databaseentry_t** database;
+
+#define DATABASESIZE  18815231    // ~3.5GB  
+
+/* Allocates memeory for database and initializes it */
+void initialize_database() {
+    database = (databaseentry_t **) malloc(sizeof(databaseentry_t *) * DATABASESIZE);
+    for (int i = 0; i < DATABASESIZE; i++) {
+        database[i] = NULL;
+    }
+    return;
+}
+
+/* Stores board positions and corresponding winrate in global database */
+void update_database_entry(board_t *board, int winner) {
+    uint64_t hash = calculate_zobrist_hash(board);
+    uint64_t key = hash % DATABASESIZE;
+
+    databaseentry_t *new = NULL;
+    // if there is no entry at key, just create a new one ... 
+    if(!database[key]) {
+        database[key] = (databaseentry_t *) malloc (sizeof(databaseentry_t));
+        new = database[key];
+    } else {
+        // otherwise, check if there already is an entry in the list with the same hash 
+        // we ignore the insanley unlikely event that two different boards have the same hash
+        databaseentry_t* cur = database[key];
+        databaseentry_t* prev = cur;
+        while(cur) {
+            // if there exists an entry for the board, update winrate
+            if(cur->hash == hash) {
+                cur->seen++;
+                if(winner == WINWHITE) cur->white_won++;
+                if(winner == WINBLACK) cur->black_won++;
+                if(winner == DRAW) cur->draw++;
+                return;
+            }
+            prev = cur;
+            cur = cur->next;
+        }
+        // if there exists no entry for that board
+        // we CREATE a new entry
+        prev->next = (databaseentry_t *) malloc (sizeof(databaseentry_t));
+        new = prev->next;
+    }
+    
+    // finally, fill the new entry with the data 
+    new->hash=hash;
+    new->next=NULL;
+
+    new->seen=1;
+    if(winner == WINWHITE) new->white_won = 1;
+    if(winner == WINBLACK) new->black_won = 1;
+    if(winner == DRAW) new->draw = 1;
+
+    return;
+}
+
+/* Get the evaluation of a board */
+void probe_database_entry(board_t* board) {
+    uint64_t hash = calculate_zobrist_hash(board);
+    uint64_t key = hash % DATABASESIZE;
+
+    databaseentry_t *cur = database[key];
+    // search the list for the entry with the same hash 
+    while(cur) {
+        // if there is one, print eval
+        if(cur->hash == hash) {
+            fprintf(stderr, "\n#:\t%d\nW:\t%f\nB:\t%f\nD:\t%f\n", cur->seen, (float) cur->white_won/cur->seen, (float) cur->black_won/cur->seen, (float) cur->draw/cur->seen);
+            return;
+        }
+        cur = cur->next;
+    }
+
+    fprintf(stderr, "\nNo entry found!\n");
+    return;
+}
 
 /* Returns true if character is a file (i.e a-h) */
 int is_file(char character){
@@ -523,6 +610,45 @@ int play_game(chessgame_t* chessgame){
     return 0;
 }
 
+/*  Playes all games in given chess game list and    
+    stores all chess positions and corresponding winrates */
+void load_games_into_database(chessgame_t** chessgames, int nr_of_games){
+     // play games
+    for(int i = 0; i < nr_of_games; i++){
+        if((i%1000) == 0){
+            fprintf(stderr, ".");
+        }
+        chessgame_t* chessgame = chessgames[i];
+        // (0) intialize board 
+        board_t *board = init_board();
+        load_by_FEN(board, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+        // (1) read move 
+        char* token = strtok(chessgame->movelist, " ");
+
+        do{
+            // (2) parse move
+            move_t *move = str_to_move(board, token);
+
+            // (3) play move
+            if (move){
+                do_move(board, move);
+                update_database_entry(board, chessgame->winner);
+                free_move(move);
+            }
+            else {
+                print_board(board);
+                fprintf(stderr, "%sInvalid move: %s%s\n",Color_PURPLE, token, Color_END);
+                exit(-1);
+            }
+        // (4) repeat until all moves played
+        } while((token = strtok(NULL, " ")));
+
+        free_board(board);
+    }
+    
+}
+
 /* Counts the number of games in a PGN chess game file */
 int count_number_of_games(){
     int game_count = 0;
@@ -635,7 +761,7 @@ chessgame_t** parse_chessgames_file(int nr_of_games){
     }
 
     fclose(fp);
-    printf("Nr. of games:\t%d\n", nr_of_games);
+    fprintf(stderr, "Nr. of games:\t%d\n", nr_of_games);
     return chessgames;
 }
 
@@ -646,10 +772,15 @@ int main(){
 
     // initialize chess engine
     initialize_chess_engine_only_necessary();
+    initialize_zobrist_table();
+    initialize_database();
 
-    // play games
-    for(int i = 0; i < nr_of_games; i++){
-        play_game(chessgames[i]);
-    }
-    
+    load_games_into_database(chessgames, nr_of_games);
+
+    // number of different boards = 1739062
+
+    board_t *board = init_board();
+    load_by_FEN(board, "r1bqkbnr/pppp1ppp/2n5/4p3/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq d3 0 3");
+
+    probe_database_entry(board);
 }
