6cfcf84690d7587d9bce3d7db6fa69a3ccd1fd28
Alexander Herbrich <aherbrich32@gmail.com>
2023-07-09 21:33:33 +0200

    Small increase of speed (1%) - now at 4Mn/s on test positions!

--- src/helpers.c
+++ src/helpers.c
@@ -1,6 +1,19 @@
 #include "../include/chess.h"
 #include "../include/magic.h"
 
+const int DEBRUIJN64[64] = {
+	0, 47,  1, 56, 48, 27,  2, 60,
+   57, 49, 41, 37, 28, 16,  3, 61,
+   54, 58, 35, 52, 50, 42, 21, 44,
+   38, 32, 29, 23, 17, 11,  4, 62,
+   46, 55, 26, 59, 40, 36, 15, 53,
+   34, 51, 20, 43, 31, 22, 10, 45,
+   25, 39, 14, 33, 19, 30,  9, 24,
+   13, 18,  8, 12,  7,  6,  5, 63
+};
+
+const bitboard_t MAGICDEBRUIJN = 0x03f79d71b4cb0a89;
+
 /* Generates a pseudo-random 64bit unsigned integer */
 uint64_t random_uint64() {
     uint64_t u1, u2, u3, u4;
@@ -21,28 +34,13 @@ idx_t pos_to_idx(int row, int col) { return (row * 8 + col); }
 
 /* Returns index of least significant bit */
 int find_1st_bit(bitboard_t bb) {
-    const int lsb_64_table[64] = {
-        63, 30, 3,  32, 59, 14, 11, 33, 60, 24, 50, 9,  55, 19, 21, 34,
-        61, 29, 2,  53, 51, 23, 41, 18, 56, 28, 1,  43, 46, 27, 0,  35,
-        62, 31, 58, 4,  5,  49, 54, 6,  15, 52, 12, 40, 7,  42, 45, 16,
-        25, 57, 48, 13, 10, 39, 8,  44, 20, 47, 38, 22, 17, 37, 36, 26};
-
-    assert(bb);
-    unsigned int folded;
-    assert(bb != 0);
-    bb ^= bb - 1;
-    folded = (int)bb ^ (bb >> 32);
-    return lsb_64_table[folded * 0x78291ACF >> 26];
+    return DEBRUIJN64[MAGICDEBRUIJN * (bb ^ (bb - 1)) >> 58];
 }
 
 /* Sets the LS1B (least significant 1 bit) to 0 and returns it index */
-int pop_1st_bit(bitboard_t *bitboard) {
-    if (!(*bitboard)) {
-        return -1;
-    }
-
-    int idx = find_1st_bit(*bitboard);
-    *bitboard &= *bitboard - 1;
+int pop_1st_bit(bitboard_t *bb) {
+    int idx = find_1st_bit(*bb);
+    *bb &= *bb - 1;
     return idx;
 }
 
