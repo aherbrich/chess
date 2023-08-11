5e53ca424d681f957fd83c799c7cbb0e0dc480aa
Ralf Herbrich <rherbrich@gmail.com>
2023-08-11 15:49:14 +0200

    Added full training code

--- src/ordering/api.c
+++ src/ordering/api.c
@@ -11,6 +11,8 @@
 #include "../../include/ordering.h"
 #include "../../include/parse.h"
 
+#define EPSILON (1e-3)
+
 /* ------------------------------------------------------------------------------------------------ */
 /* functions for managing the hash-table of a urgencies for the Bayesian move ranking model         */
 /* ------------------------------------------------------------------------------------------------ */
@@ -163,9 +165,9 @@ void update(gaussian_t* urgency_beliefs, int* hashes, int no_hashes, double beta
         gaussian_mean_factor_update_to_variable(&g[i]);
     }
 
-    double delta = 1e4;
-    while (delta > 1e-3) {
-        delta = 0.0;
+    double delta = EPSILON;
+    while (delta >= EPSILON) {
+        delta = 0;
 
         for (int i = 0; i < no_hashes - 1; ++i) {
             delta = fmax(delta, weighted_sum_factor_update_to_sum(&s[i]));
@@ -180,6 +182,143 @@ void update(gaussian_t* urgency_beliefs, int* hashes, int no_hashes, double beta
         urgency_beliefs[hashes[i]] = urgency[i];
     }
 
+    return;
+}
+
+/* ------------------------------------------------------------------------------------------------ */
+/* functions for batch training of a Bayesian move ranking model                                    */
+/* ------------------------------------------------------------------------------------------------ */
+
+/* adds the factor graph that processes a single move made to the urgency belief distributions indexed by the no_hashes many move hashes given in hashes */
+ranking_update_info_t* add_ranking_update_graph(ranking_update_info_t* root, gaussian_t* urgency_beliefs, int* hashes, int no_hashes, double beta_squared) {
+    ranking_update_info_t* info = (ranking_update_info_t*) malloc(sizeof(ranking_update_info_t));
+
+    /* copy the number of moves */
+    info->no_moves = no_hashes;
+
+    /* allocate memory for the factors */
+    info->g = (gaussian_mean_factor_info_t*) malloc(no_hashes * sizeof(gaussian_mean_factor_info_t));
+    info->s = (weighted_sum_factor_info_t*) malloc((no_hashes - 1) * sizeof(weighted_sum_factor_info_t));
+    info->h = (greater_than_factor_info_t*) malloc((no_hashes - 1) * sizeof(greater_than_factor_info_t));
+
+    info->latent_urgency = (gaussian_t*) malloc(no_hashes * sizeof(gaussian_t));
+    info->msg_from_g_to_latent_urgency = (gaussian_t*) malloc(no_hashes * sizeof(gaussian_t));
+    info->msg_from_g_to_urgency = (gaussian_t*) malloc(no_hashes * sizeof(gaussian_t));
+    info->diffs = (gaussian_t*) malloc((no_hashes - 1) * sizeof(gaussian_t));
+    info->msg_from_s_to_diffs = (gaussian_t*) malloc((no_hashes - 1) * sizeof(gaussian_t));
+    info->msg_from_s_to_top_urgency = (gaussian_t*) malloc((no_hashes - 1) * sizeof(gaussian_t));
+    info->msg_from_s_to_urgency = (gaussian_t*) malloc((no_hashes - 1) * sizeof(gaussian_t));
+    info->msg_from_h_to_diffs = (gaussian_t*) malloc((no_hashes - 1) * sizeof(gaussian_t));
+
+    /* wire the factors correctly */
+    for (int i = 0; i < no_hashes; i++) {
+        info->g[i].var_marginal = &info->latent_urgency[i];
+        info->g[i].mean_marginal = &urgency_beliefs[hashes[i]];
+        info->g[i].var_msg = &info->msg_from_g_to_latent_urgency[i];
+        info->g[i].mean_msg = &info->msg_from_g_to_urgency[i];
+        info->g[i].beta_squared = beta_squared;
+
+        if (i < no_hashes - 1) {
+            info->s[i].a1 = 1.0;
+            info->s[i].a2 = -1.0;
+            info->s[i].s1_marginal = &info->latent_urgency[0];
+            info->s[i].s2_marginal = &info->latent_urgency[i + 1];
+            info->s[i].sum_marginal = &info->diffs[i];
+            info->s[i].s1_msg = &info->msg_from_s_to_top_urgency[i];
+            info->s[i].s2_msg = &info->msg_from_s_to_urgency[i];
+            info->s[i].sum_msg = &info->msg_from_s_to_diffs[i];
+
+            info->h[i].epsilon = 0.0;
+            info->h[i].marginal = &info->diffs[i];
+            info->h[i].msg = &info->msg_from_h_to_diffs[i];
+        }
+    }
+
+    /* initialize the messages and marginals correctly */
+    for (int i = 0; i < no_hashes; i++) {
+        info->latent_urgency[i] = init_gaussian1D(0,0);
+        info->msg_from_g_to_latent_urgency[i] = init_gaussian1D(0,0);
+        info->msg_from_g_to_urgency[i] = init_gaussian1D(0,0);
+        if (i < no_hashes - 1) {
+            info->diffs[i] = init_gaussian1D(0,0);
+            info->msg_from_s_to_diffs[i] = init_gaussian1D(0,0);
+            info->msg_from_s_to_top_urgency[i] = init_gaussian1D(0,0);
+            info->msg_from_s_to_urgency[i] = init_gaussian1D(0,0);
+            info->msg_from_h_to_diffs[i] = init_gaussian1D(0,0);
+        }
+    }
+
+    /* make it the new root */
+    info->next = root;
+    return (info);
+}
+
+/* run message passing on the whole graph until convergence */
+void refresh_update_graph(ranking_update_info_t *root) {
+    double outer_delta = EPSILON;
+    while (outer_delta >= EPSILON) {
+        outer_delta = 0.0;
+
+        ranking_update_info_t* ranking_update = root;
+        while (ranking_update) {
+            /* send message to latent urgencies */
+            for (int i = 0; i < ranking_update->no_moves; ++i) {
+                outer_delta = fmax(outer_delta, gaussian_mean_factor_update_to_variable(&ranking_update->g[i]));
+                // fprintf(stderr, "   prior %d: %f +- %f\n", i, mean(*ranking_update->g[i].mean_marginal), sqrt(variance(*ranking_update->g[i].mean_marginal)));
+            }
+
+            /* iterate the ranking factors until convergence */
+            double delta = EPSILON;
+            while (delta >= EPSILON) {
+                delta = 0.0;
+
+                for (int i = 0; i < ranking_update->no_moves - 1; ++i) {
+                    delta = fmax(delta, weighted_sum_factor_update_to_sum(&ranking_update->s[i]));
+                    delta = fmax(delta, greater_than_factor_update(&ranking_update->h[i]));
+                    delta = fmax(delta, weighted_sum_factor_update_to_summand1(&ranking_update->s[i]));
+                    delta = fmax(delta, weighted_sum_factor_update_to_summand2(&ranking_update->s[i]));
+                }
+            }
+
+            /* send message back to the actual urgencies */
+            for (int i = 0; i < ranking_update->no_moves; ++i) {
+                outer_delta = fmax(outer_delta, gaussian_mean_factor_update_to_mean(&ranking_update->g[i]));
+                // fprintf(stderr, "   posterior %d: %f +- %f\n", i, mean(*ranking_update->g[i].mean_marginal), sqrt(variance(*ranking_update->g[i].mean_marginal)));
+            }
+
+            /* move to next update */
+            ranking_update = ranking_update->next;
+        }
+
+        fprintf(stderr, "outer delta: %f\n", outer_delta);
+    }
+
+    return;
+}
+
+/* deletes the linked list of ranking updates */
+void delete_ranking_update_graphs(ranking_update_info_t* root) {
+    ranking_update_info_t* next;
+    while (root != NULL) {
+        next = root->next;
+
+        /* free all the factors and messages */
+        free(root->g);
+        free(root->s);
+        free(root->h);
+        free(root->latent_urgency);
+        free(root->msg_from_g_to_latent_urgency);
+        free(root->msg_from_g_to_urgency);
+        free(root->diffs);
+        free(root->msg_from_s_to_diffs);
+        free(root->msg_from_s_to_top_urgency);
+        free(root->msg_from_s_to_urgency);
+        free(root->msg_from_h_to_diffs);
+
+        /* free root itself */
+        free(root);
+        root = next;
+    }
     return;
 }
 
--- src/train_ordering.c
+++ src/train_ordering.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "../include/engine.h"
 #include "../include/factors.h"
@@ -8,7 +9,12 @@
 #include "../include/ordering.h"
 #include "../include/parse.h"
 
-void train_model(chessgame_t** chessgames, int nr_of_games) {
+/* trains a Bayesian ranking model from the replay of the games */
+void train_model(chessgame_t** chessgames, int nr_of_games, int full_training) {
+    ranking_update_info_t *ranking_updates = NULL;
+
+    fprintf(stderr, "Training started (%s)\n", (full_training) ? "full" : "incremental");
+
     /* play games */
     for (int i = 0; i < nr_of_games; i++) {
         chessgame_t* chessgame = chessgames[i];
@@ -49,7 +55,11 @@ void train_model(chessgame_t** chessgames, int nr_of_games) {
                 }
 
                 // execute_ranking update
-                update(ht_urgencies, urgencies_indices, nr_of_moves, 0.5 * 0.5);
+                if (full_training) {
+                    ranking_updates = add_ranking_update_graph(ranking_updates, ht_urgencies, urgencies_indices, nr_of_moves, 0.5 * 0.5);
+                } else {
+                    update(ht_urgencies, urgencies_indices, nr_of_moves, 0.5 * 0.5);
+                }
 
                 /* execute MADE_MOVE */
                 do_move(board, move);
@@ -66,6 +76,12 @@ void train_model(chessgame_t** chessgames, int nr_of_games) {
         free_board(board);
     }
 
+    /* now perform the full training and release the small update (factor) graphs */
+    if (full_training) {
+        refresh_update_graph(ranking_updates);
+        delete_ranking_update_graphs(ranking_updates);
+    }
+
     /* free chess games */
     for (int i = 0; i < nr_of_games; i++) {
         free(chessgames[i]->movelist);
@@ -74,7 +90,39 @@ void train_model(chessgame_t** chessgames, int nr_of_games) {
     free(chessgames);
 }
 
-int main() {
+int main(int argc, char** argv) {
+    int full_training = 0;
+    int test_read_write_model = 0;
+
+    /* command line parsing using getopt */
+    int c;
+    while ((c = getopt(argc, argv, "fht")) != -1) {
+        switch (c) {
+            case 'h':
+                printf("Usage: %s\n", argv[0]);
+                printf("Train the model for move ordering\n");
+                printf("Options:\n");
+                printf("  -h\t\t\tShow this help message\n");
+                printf("  -t\t\t\tTest the write and load function for models\n");
+                printf("  -f\t\t\tUse full factor graph training\n");
+                exit(0);
+            case 'f':
+                full_training = 1;
+                break;
+            case 't':
+                test_read_write_model = 1;
+                break;
+            default:
+                fprintf(stderr, "Unknown option: %c\n", c);
+                exit(-1);
+        }
+    }
+
+    /* output the options used */
+    printf("Options:\n");
+    printf("  Full Training: %s\n", (full_training)?"yes":"no");
+    printf("  Test model read-write: %s\n", (test_read_write_model)?"yes":"no");
+
     /* parse chess game file */
     int nr_of_games = count_number_of_games();
     chessgame_t** chessgames = parse_chessgames_file(nr_of_games);
@@ -83,7 +131,7 @@ int main() {
     initialize_chess_engine_necessary();
 
     /* train the model */
-    train_model(chessgames, nr_of_games);
+    train_model(chessgames, nr_of_games, full_training);
 
     /* write the mode to a binary file */
     write_ht_urgencies_to_binary_file("ht_urgencies.bin", ht_urgencies);
@@ -97,28 +145,32 @@ int main() {
     }
     fprintf(stderr, "Unique moves: %d\n", count);
 
-    /* test that we have written the same model as we have in memory */
-    gaussian_t* ht_urgencies_test = initialize_ht_urgencies();
-    load_ht_urgencies_from_binary_file("ht_gaussians.bin", ht_urgencies_test);
-    for (int i = 0; i < HT_GAUSSIAN_SIZE; i++) {
-        if (mean(ht_urgencies[i]) != mean(ht_urgencies_test[i]) ||
-            variance(ht_urgencies[i]) != variance(ht_urgencies_test[i])) {
-            fprintf(stderr, "Error: ht_urgencies[%d] is not the same\n", i);
-            fprintf(stderr, "mean(ht_urgencies[%d]) = %f\n", i,
-                    mean(ht_urgencies[i]));
-            fprintf(stderr, "mean(ht_urgencies_test[%d]) = %f\n", i,
-                    mean(ht_urgencies_test[i]));
-            fprintf(stderr, "variance(ht_urgencies[%d]) = %f\n", i,
-                    variance(ht_urgencies[i]));
-            fprintf(stderr, "variance(ht_urgencies_test[%d]) = %f\n", i,
-                    variance(ht_urgencies_test[i]));
-            exit(-1);
+    if (test_read_write_model) {
+        /* test that we have written the same model as we have in memory */
+        gaussian_t* ht_urgencies_test = initialize_ht_urgencies();
+        load_ht_urgencies_from_binary_file("ht_urgencies.bin", ht_urgencies_test);
+        for (int i = 0; i < HT_GAUSSIAN_SIZE; i++) {
+            if (mean(ht_urgencies[i]) != mean(ht_urgencies_test[i]) ||
+                variance(ht_urgencies[i]) != variance(ht_urgencies_test[i])) {
+                fprintf(stderr, "Error: ht_urgencies[%d] is not the same\n", i);
+                fprintf(stderr, "mean(ht_urgencies[%d]) = %f\n", i,
+                        mean(ht_urgencies[i]));
+                fprintf(stderr, "mean(ht_urgencies_test[%d]) = %f\n", i,
+                        mean(ht_urgencies_test[i]));
+                fprintf(stderr, "variance(ht_urgencies[%d]) = %f\n", i,
+                        variance(ht_urgencies[i]));
+                fprintf(stderr, "variance(ht_urgencies_test[%d]) = %f\n", i,
+                        variance(ht_urgencies_test[i]));
+                exit(-1);
+            }
         }
+
+        /* free the memory for the hash-table of urgencies */
+        deletes_ht_urgencies(ht_urgencies_test);
     }
 
     /* free the memory for the hash-table of urgencies */
     deletes_ht_urgencies(ht_urgencies);
-    deletes_ht_urgencies(ht_urgencies_test);
 
     return 0;
 }
