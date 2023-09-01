32d7d0e5baa0daf5be790b8fc073f385207bcb8a
Alexander Herbrich <aherbrich32@gmail.com>
2023-09-01 10:43:47 +0200

    Temporarily disabled leak check to see if runner works then

--- test-script.sh
+++ test-script.sh
@@ -18,17 +18,17 @@ echo "\n\033[0;35mm====================================== [test_tt] ============
 # ./build/tests/test_movegen $? -eq 0 && echo "\n<<< \033[0;32mOK\033[0m" || echo "\n<<< \033[0;31mFAIL\033[0m";
 
 # run tests with leak check
-echo "Running tests with leak check..."
-echo "\n\033[0;35mm====================================== [test_factor_graph + leak check] ======================================\033[0m\n"
-leaks -atExit -- ./build/tests/test_factor_graph
-echo "\n\033[0;35mm====================================== [test_eval + leak check] ======================================\033[0m\n"
-leaks -atExit -- ./build/tests/test_eval 
-echo "\n\033[0;35mm====================================== [test_perftdivide + leak check] ======================================\033[0m\n"
-leaks -atExit -- ./build/tests/test_perftdivide
-echo "\n\033[0;35mm====================================== [test_tt + leak check] ======================================\033[0m\n"
-leaks -atExit -- ./build/tests/test_tt
-# echo "\n\033[0;35mm====================================== [test_movegen + leak check] ======================================\033[0m\n"
-# leaks -atExit -- ./build/tests/test_movegen
+# echo "Running tests with leak check..."
+# echo "\n\033[0;35mm====================================== [test_factor_graph + leak check] ======================================\033[0m\n"
+# leaks -atExit -- ./build/tests/test_factor_graph
+# echo "\n\033[0;35mm====================================== [test_eval + leak check] ======================================\033[0m\n"
+# leaks -atExit -- ./build/tests/test_eval 
+# echo "\n\033[0;35mm====================================== [test_perftdivide + leak check] ======================================\033[0m\n"
+# leaks -atExit -- ./build/tests/test_perftdivide
+# echo "\n\033[0;35mm====================================== [test_tt + leak check] ======================================\033[0m\n"
+# leaks -atExit -- ./build/tests/test_tt
+# # echo "\n\033[0;35mm====================================== [test_movegen + leak check] ======================================\033[0m\n"
+# # leaks -atExit -- ./build/tests/test_movegen
 
 # try to compile all targets with -Werror and run tests with sanitizers activated
 make clean && make sanitize
